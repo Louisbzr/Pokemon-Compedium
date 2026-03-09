8fb20af8202c13dd0ebefba91739a9bbf78ef328
Louis Brazeau <louis.brazeau.pro@gmail.com>
2026-03-09 14:01:48 +0100

    fix import

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const pokemonService = require('./services/pokemonService');
 const authRoutes = require('./routes/auth');
-const authMiddleware = require('./middlewares/authMiddleware'); 
+const authMiddleware = require('./middleware/authMiddleware'); 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
