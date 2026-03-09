4382307b3bdc60506377e7b0d1cd393aa7522adf
Louis Brazeau <louis.brazeau.pro@gmail.com>
2026-03-09 20:14:24 +0100

    server

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const host = '::'; 
 
+app.options('*', cors());
+
 const allowedOrigins = (process.env.FRONT_ORIGINS || 
   'http://localhost:3000').split(',');
 
